847e810782211b7d1a7e0a56467706cadd0798ed
venom <venom@DESKTOP-VI6EVSE>
2020-07-15 21:46:37 -0500

    Another change

---
+++ Persona.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SimulacionEconomica
+{
+    class Persona
+    {
+        //Propiedades
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        //Métodos (Comportamiento)
+        public string Saludar(string Nombre)
+        {
+
+            return $"Hola {Nombre} me llamo {this.Nombre} ";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimulacionEconomica
+{
+    class Program
+    {
+        public static void Main(string []args)
+        {
+            Console.WriteLine("Hola mundo!");
+
+            int id = 1;
+            string nombre = "Darwin";
+
+            Persona investigador = new Persona
+            {
+                Id = id,
+                Nombre = nombre
+            };
+
+            string personaLaQueSaludo = "Gorky";
+            string saludo = investigador.Saludar(personaLaQueSaludo);
+
+            Console.WriteLine(saludo);
+            Console.ReadKey();
+        }
+    }
+}
